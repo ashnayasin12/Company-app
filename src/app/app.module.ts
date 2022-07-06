dc33572057fcbd50246b50ab9b968b99115cf82c
ashnayasin12 <ashnayasin12@gmail.com>
2022-07-06 13:12:37 +0530

    companyname created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompanynameComponent } from './companyname/companyname.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanynameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/companyname/companyname.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-companyname',
+  templateUrl: './companyname.component.html',
+  styleUrls: ['./companyname.component.css']
+})
+export class CompanynameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
